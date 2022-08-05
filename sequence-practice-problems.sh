33433f4ff9dadf2ae983255127253ee68050f862
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-08-05 20:13:45 +0530

    two dice addition

--- sequence-practice-problems.sh
+++ sequence-practice-problems.sh
@@ -4,4 +4,11 @@ echo $((RANDOM%10))
 echo dice 1 - 6 number
 echo $((1 + RANDOM%6))
 
+echo two dice addition
+dice1=$((RANDOM%6))
+dice2=$((RANDOM%6))
+     sum=$((dice1+dice2))
+     	echo=$sum
+
+
 
