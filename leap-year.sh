c67a4a1c51876e8ed4e39beeb4f2529a776758c9
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-08-07 18:59:37 +0530

    leap year program

---
+++ leap-year.sh
@@ -0,0 +1,21 @@
+\echo "Enter the year"
+
+read year
+
+x=`expr $year % 400`
+
+y=`expr $year % 100`
+
+z=`expr $year % 4`
+
+if [ $x -eq 0 ] || [ $y -ne 0 ] && [ $z –eq 0];
+
+then
+
+echo " Entered year - $year is a leap year"
+
+else
+
+echo "Entered year - $year is not a leap year "
+
+fi
